e2f26cbf05212d9d8a8a468bd4d64dc3921b500b
Jaime Molina <js.molina13@uniandes.edu.co>
2018-02-08 15:00:45 -0500

    casper functional

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const CONSTANTS = require('./src/constants')
 const config = require('./src/config')
-const casperjs  = require('casperjs').create(config)
+const casper  = require('casper').create(config)
 // const platform = require('./src/platform')
-const {
-  INSTAGRAM_URL,
-  LOGIN_FORM,
-  LOGIN_LINK
-} = CONSTANTS
+const INSTAGRAM_URL  = CONSTANTS.INSTAGRAM_URL
+const LOGIN_FORM  = CONSTANTS.LOGIN_FORM
+const LOGIN_LINK  = CONSTANTS.LOGIN_LINK
 
 // print out all the messages in the headless browser context
-casper.on('remote.message', (msg) => {
+casper.on('remote.message', function (msg)  {
   this.echo('remote message caught: ' + msg);
 });
 
 // print out all the messages in the headless browser context
-casper.on("page.error", (msg, trace) => {
+casper.on("page.error", function (msg, trace) {
   this.echo("Page Error: " + msg, "ERROR");
 });
 
-casper.start(INSTAGRAM_URL, () => {
+casper.start(INSTAGRAM_URL, function () {
   console.log("page instagram loaded");
 });
 
-casper.then(() => {
+casper.then(function () {
   // Click on 1st result link
-  this.click(`${LOGIN_LINK} a`);
+  this.click(LOGIN_LINK + ' a');
 });
 
-casper.then(() => {
+casper.then( function() {
     this.fill(LOGIN_FORM, {
         'username':    'I am watching you',
         'password':    'I am watching you',
     }, true);
 });
+
+casper.run()
