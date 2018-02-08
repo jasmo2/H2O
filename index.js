11130d089173847d988b8318dbd6061a9fb7f701
jasmo2 <jasmo2.com@gmail.com>
2018-02-07 21:19:10 -0500

    config

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const CONSTANTS = require('./src/constants')
-const casperjs  = require('casperjs').create()
-
+const config = require('./src/config')
+const casperjs  = require('casperjs').create(config)
 // const platform = require('./src/platform')
-const {INSTAGRAM_URL, LOGIN_LINK} = CONSTANTS
+const {
+  INSTAGRAM_URL,
+  LOGIN_FORM,
+  LOGIN_LINK
+} = CONSTANTS
 
-casper.start(INSTAGRAM_URL, function() {
-   // Wait for the page to be loaded
-   this.waitForSelector('form[action="/search"]');
+// print out all the messages in the headless browser context
+casper.on('remote.message', (msg) => {
+  this.echo('remote message caught: ' + msg);
+});
+
+// print out all the messages in the headless browser context
+casper.on("page.error", (msg, trace) => {
+  this.echo("Page Error: " + msg, "ERROR");
+});
+
+casper.start(INSTAGRAM_URL, () => {
+  console.log("page instagram loaded");
+});
+
+casper.then(() => {
+  // Click on 1st result link
+  this.click(`${LOGIN_LINK} a`);
+});
+
+casper.then(() => {
+    this.fill(LOGIN_FORM, {
+        'username':    'I am watching you',
+        'password':    'I am watching you',
+    }, true);
 });
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 module.exports = {
     INSTAGRAM_URL: 'https://www.instagram.com/',
+    LOGIN_FORM: '_3jvtb',
     LOGIN_LINK: '_g9ean'
 }
