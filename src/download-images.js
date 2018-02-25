4515de1dbd1ae2da1e40a6e4e965a1e598feef9c
jasmo2 <jasmo2.com@gmail.com>
2018-02-25 18:37:33 -0500

    log downloaded images

--- src/download-images.js
+++ src/download-images.js
@@ -1,7 +1,13 @@
 const download = require('image-downloader')
+const getPercentage = require('./utils')
 
 function downloadImages(imagesUrls, dest) {
-  const downloads = imagesUrls.map(url => download.image({ url, dest }))
+  let i = 0
+  const imagesUrlsLength = imagesUrls.length
+  const downloads = imagesUrls.map(url => {
+    console.log('Image downloading', getPercentage(imagesUrlsLength, ++i))
+    return download.image({ url, dest })
+  })
   return Promise.all(downloads)
 }
 
